26ff20cbaed9a3b1712b6cc98281d0419e791c39
PJeremie7 <jeremie.planta@stahl-holding-saar.de>
2021-09-16 14:29:38 +0200

    feat: added the PrimitiveDatentypen module from Tutorial Video 3

---
+++ PrimitiveDatentypen/src/srcpackage/PrimitiveDatentypen.java
@@ -0,0 +1,14 @@
+package srcpackage;
+
+public class PrimitiveDatentypen {
+    public static void main(String[] args){
+
+        byte byteMax = 127;
+        boolean wahrOderFalsch = true;
+        double test = 3.54667887687686843535423563436;
+
+        System.out.println(byteMax);
+        System.out.println(wahrOderFalsch);
+        System.out.println(test);
+    }
+}
