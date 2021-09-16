fcff10b280af835384b682a3a3ceb7f77cd7855a
PJeremie7 <jeremie.planta@stahl-holding-saar.de>
2021-09-16 14:39:24 +0200

    feat: added the KlasseString module from Tutorial Video 4

---
+++ KlassseString/src/srcpackage/KlasseString.java
@@ -0,0 +1,11 @@
+package srcpackage;
+
+public class KlasseString {
+
+    public static void main(String[] args) {
+
+        String string = "Ich bin eine Zeichenkette";
+
+        System.out.println(string + "!");
+    }
+}
