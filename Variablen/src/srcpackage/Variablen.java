a9e3e866ca01042a07c401e0856c7bad6ff93041
PJeremie7 <jeremie.planta@stahl-holding-saar.de>
2021-09-16 14:06:36 +0200

    feat: added the Variablen module from Tutorial Video 2

---
+++ Variablen/src/srcpackage/Variablen.java
@@ -0,0 +1,20 @@
+package srcpackage;
+
+public class Variablen
+{
+
+    public static void main(String[] args){
+
+        int alter = 99; //Init
+
+        int alter2; //Deklaration
+        alter2 = 9; //Zuweisung
+
+        System.out.println(alter2);
+
+        alter2 = 77;
+
+        System.out.println(alter2);
+        System.out.println(alter);
+    }
+}
