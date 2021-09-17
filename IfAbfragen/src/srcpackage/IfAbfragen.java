c81381019cac416d418e5b65dd14231152d9c987
PJeremie7 <jeremie.planta@stahl-holding-saar.de>
2021-09-17 07:56:17 +0200

    feat: added the IfAbfragen module from Tutorial Video 7

---
+++ IfAbfragen/src/srcpackage/IfAbfragen.java
@@ -0,0 +1,16 @@
+package srcpackage;
+
+public class IfAbfragen {
+
+    public static void main(String[] args) {
+
+        int gewicht = 150;
+
+        if(gewicht > 120){
+            System.out.println("Die Wage ist zerbrochen");
+        } else{
+            System.out.println("Batterien wechseln");
+        }
+
+    }
+}
