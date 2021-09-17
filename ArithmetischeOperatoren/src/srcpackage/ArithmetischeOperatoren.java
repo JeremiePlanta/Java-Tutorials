3bc6d85b1052342ba43df8dd880e3460f66dbde5
PJeremie7 <jeremie.planta@stahl-holding-saar.de>
2021-09-17 07:44:17 +0200

    feat: added the ArithmetischeOperatoren module from Tutorial Video 5

---
+++ ArithmetischeOperatoren/src/srcpackage/ArithmetischeOperatoren.java
@@ -0,0 +1,17 @@
+package srcpackage;
+
+public class ArithmetischeOperatoren
+{
+
+    public static void main(String[] args) {
+
+        int addition = 3 + 4;
+        int nummer1 = 7;
+        int nummer2 = 8;
+        
+
+        System.out.println(addition);
+        System.out.println(nummer1 + nummer2);
+
+    }
+}
