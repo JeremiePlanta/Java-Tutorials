d995c27353d5f279baf36e4af57e85e44c69d6c1
PJeremie7 <jeremie.planta@stahl-holding-saar.de>
2021-09-17 13:39:34 +0200

    feat: added the WhileSchleife module from Tutorial Video 9

---
+++ WhileSchleife/src/srcpackage/WhileSchleife.java
@@ -0,0 +1,14 @@
+package srcpackage;
+
+public class WhileSchleife {
+
+    public static void main(String[] args) {
+
+        int tore = 0;
+
+        while (tore < 7){
+            tore ++;
+        }
+        System.out.println("Deutschland gewinnt "+ tore +":1 gegen Brasilien.");
+    }
+}
