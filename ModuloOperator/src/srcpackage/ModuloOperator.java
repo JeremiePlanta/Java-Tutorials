12efbe84a2b94ba0fc45c6d6ec6519ad7f2e7a88
PJeremie7 <jeremie.planta@stahl-holding-saar.de>
2021-09-17 07:47:58 +0200

    feat: added the ModuloOperator module from Tutorial Video 6

---
+++ ModuloOperator/src/srcpackage/ModuloOperator.java
@@ -0,0 +1,12 @@
+package srcpackage;
+
+public class ModuloOperator {
+
+    public static void main(String[] args) {
+
+        int nummer1 = 72 % 5;
+
+        System.out.println(nummer1);
+
+    }
+}
