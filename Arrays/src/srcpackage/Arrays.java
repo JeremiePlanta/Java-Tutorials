32aa9cc9a4554a09325fac282b60c8ca60d2b381
PJeremie7 <jeremie.planta@stahl-holding-saar.de>
2021-09-21 09:25:26 +0200

    feat: added the Arrays module from Tutorial Video 13

---
+++ Arrays/src/srcpackage/Arrays.java
@@ -0,0 +1,13 @@
+package srcpackage;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+
+        String[] lannisters = {"Tyrion","Jaime","Tywin","Cersei","Kevan"};
+
+        for(String character : lannisters ){
+            System.out.println(character);
+        }
+    }
+}
