60bede4a3c8052d214dcaa425fb690072f2480c8
PJeremie7 <jeremie.planta@stahl-holding-saar.de>
2021-09-21 14:58:53 +0200

    style: added a comment for a better understanding

--- Arrays/src/srcpackage/Arrays.java
+++ Arrays/src/srcpackage/Arrays.java
@@ -4,6 +4,18 @@ public class Arrays {
 
     public static void main(String[] args) {
 
+        String[] names = new String[3]; //festlegung der Größe des Arrays
+
+        names[0] = "Karlsson";
+        names[1] = "Karlssonsson";
+        names[2] = "Karlssonssonsson";
+        
+        //Ausgabe der elemente des arrays über einen for Loop
+        for(int i =0; i < names.length; i++){
+            System.out.println(names[i]);
+        }
+
+
         String[] lannisters = {"Tyrion","Jaime","Tywin","Cersei","Kevan"};
 
         for(String character : lannisters ){
--- ForeachSchleifen/src/srcpackage/ForeachSchleife.java
+++ ForeachSchleifen/src/srcpackage/ForeachSchleife.java
@@ -4,9 +4,14 @@ public class ForeachSchleife {
 
     public static void main(String[] args) {
 
-        //foreach kann nur Lesen
         String[] cryptos = {"Bitcoin", "Ethereum", "Dogecoin", "PI"};
 
+        //Dieser for Loop gibt die Elemente des cryptos array wieder und ruft dafür die Länge des arrays ab.
+        for(int i = 0; i < cryptos.length; i++){
+            System.out.println(cryptos[i]);
+        }
+
+        //Dieser foreach Loop gibt die Elemente des cryptos array wieder indem er eine Kopie des Elementes weiter gibt.
         for(String crypto : cryptos){
             System.out.println(crypto);
         }
