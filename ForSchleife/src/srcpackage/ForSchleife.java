8f758fc3e09fd8bc2fa3f8096f4e1bcde82068fb
PJeremie7 <jeremie.planta@stahl-holding-saar.de>
2021-09-21 08:11:12 +0200

    feat: added the ForSchleife module from Tutorial Video 11

---
+++ ForSchleife/src/srcpackage/ForSchleife.java
@@ -0,0 +1,11 @@
+package srcpackage;
+
+public class ForSchleife {
+
+    public static void main(String[] args) {
+        
+        for(String inLoopVariable = "Glänzend"; inLoopVariable.equals("Glänzend"); inLoopVariable = "Zerstört"){
+            System.out.println(inLoopVariable);
+        }
+    }
+}
