9b52b8c3d95b47f92d30d840bd372563547bfd44
PJeremie7 <jeremie.planta@stahl-holding-saar.de>
2021-10-13 08:58:45 +0200

    feat: added the Klassen module from Tutorial Video 19

---
+++ Klassen/src/srcpackage/Klassen.java
@@ -0,0 +1,9 @@
+package srcpackage;
+
+public class Klassen {
+
+    public static void main(String[] args) {
+        Tesla auto = new Tesla();
+        auto.fahr();
+    }
+}
---
+++ Klassen/src/srcpackage/Tesla.java
@@ -0,0 +1,12 @@
+package srcpackage;
+
+public class Tesla {
+
+    //Attribute
+    private int pferdestaerke = 180;
+
+    //Methode
+    public void fahr(){
+        System.out.println("Das Auto ist "+pferdestaerke*3 +" Meter weit gefahren.");
+    }
+}
